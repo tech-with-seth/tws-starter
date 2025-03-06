cf5f6a9e1b58be8535f1aa40b3192babe0e7a7cb
Seth Davis <sethdavis512@gmail.com>
2025-03-06 12:53:38 -0600

    Add initial Prisma setup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,27 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { prisma } from '~/db.server';
+import type { Route } from './+types/home';
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        { title: 'New React Router App' },
+        { name: 'description', content: 'Welcome to React Router!' }
+    ];
 }
 
-export default function Home() {
-  return <Welcome />;
+export async function loader({ request }: Route.LoaderArgs) {
+    const users = await prisma.user.findMany();
+
+    return {
+        users
+    };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+    return (
+        <>
+            <pre>
+                <code>{JSON.stringify(loaderData, null, 4)}</code>
+            </pre>
+        </>
+    );
 }
