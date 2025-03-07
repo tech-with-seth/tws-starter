3dcc6d5d1ec372f557187a5945f72366edcec55b
Seth Davis <sethdavis512@gmail.com>
2025-03-07 14:35:12 -0600

    Add containers

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -18,20 +18,20 @@ export async function loader() {
 
 export default function Home({ loaderData }: Route.ComponentProps) {
   return (
-    <>
-      <div className="p-4">
+    <div className="space-y-4">
+      <div className="py-4">
         <h1 className="text-4xl font-bold">Welcome to TWS Starter</h1>
       </div>
       <div className="flex gap-4">
-        <div className="p-4">
-          <h1 className="text-4xl font-bold">Side bar</h1>
+        <div>
+          <h4 className="text-xl font-bold">Side bar</h4>
         </div>
-        <div className="p-4">
+        <div>
           <pre>
             <code>{JSON.stringify(loaderData, null, 4)}</code>
           </pre>
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- app/routes/wrapper.tsx
+++ app/routes/wrapper.tsx
@@ -3,11 +3,17 @@ import { Outlet } from "react-router";
 export default function WrapperRoute() {
   return (
     <>
-      <header className="border-b p-4">Header</header>
+      <header className="border-b p-4">
+        <nav className="container mx-auto">Header</nav>
+      </header>
       <main>
-        <Outlet />
+        <div className="container mx-auto">
+          <Outlet />
+        </div>
       </main>
-      <footer className="border-t p-4">Footer</footer>
+      <footer className="border-t p-4">
+        <div className="container mx-auto">Footer</div>
+      </footer>
     </>
   );
 }
