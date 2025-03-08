6f9ac46019bbc870e596a3a233ea6c69fa9e291f
Seth Davis <sethdavis512@gmail.com>
2025-03-08 17:03:23 -0600

    Add shadcn. Add routes. 10/10. Love it.

--- app/routes/wrapper.tsx
+++ app/routes/wrapper.tsx
@@ -1,19 +1,44 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
+import { AppSidebar } from "~/components/AppSidebar";
+import { SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
 
 export default function WrapperRoute() {
+  const isLoggedIn = true;
+
   return (
-    <>
-      <header className="border-b p-4">
-        <nav className="container mx-auto">Header</nav>
-      </header>
-      <main>
-        <div className="container mx-auto">
-          <Outlet />
-        </div>
-      </main>
-      <footer className="border-t p-4">
-        <div className="container mx-auto">Footer</div>
-      </footer>
-    </>
+    <SidebarProvider defaultOpen={isLoggedIn}>
+      <AppSidebar />
+      <div className="grid h-screen w-full grid-cols-12 grid-rows-[auto_1fr_auto]">
+        <header className="col-span-full border-b">
+          <nav className="container mx-auto flex justify-between p-4">
+            <ul className="flex items-center gap-4">
+              {isLoggedIn && (
+                <li>
+                  <SidebarTrigger />
+                </li>
+              )}
+              <li>
+                <Link className="font-black" to="/">
+                  TWS
+                </Link>
+              </li>
+            </ul>
+            <ul className="flex items-center gap-4">
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="col-span-full">
+          <div className="container mx-auto p-4">
+            <Outlet />
+          </div>
+        </main>
+        <footer className="col-span-full border-t">
+          <div className="container mx-auto p-4">Footer</div>
+        </footer>
+      </div>
+    </SidebarProvider>
   );
 }
