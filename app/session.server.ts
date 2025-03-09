d182089dafebf0d429c2742a869626e39cf2ac33
Seth Davis <sethdavis512@gmail.com>
2025-03-08 22:13:33 -0600

    Update secrets import

--- app/session.server.ts
+++ app/session.server.ts
@@ -4,7 +4,7 @@ import invariant from "tiny-invariant";
 import type { User } from "~/models/user.server";
 import { getUserById } from "~/models/user.server";
 
-invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
+invariant(import.meta.env.SESSION_SECRET, "SESSION_SECRET must be set");
 
 export const sessionStorage = createCookieSessionStorage({
   cookie: {
@@ -12,7 +12,7 @@ export const sessionStorage = createCookieSessionStorage({
     httpOnly: true,
     path: "/",
     sameSite: "lax",
-    secrets: [process.env.SESSION_SECRET],
+    secrets: [import.meta.env.SESSION_SECRET],
     secure: process.env.NODE_ENV === "production",
   },
 });
