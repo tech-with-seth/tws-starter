3e3341b2e6d0a44bbd5732896e52040644815e8a
Seth Davis <sethdavis512@gmail.com>
2025-05-30 15:43:48 -0500

    Update route naming and file structure

--- app/routes.ts
+++ app/routes.ts
@@ -8,23 +8,30 @@ import {
 
 export default [
   layout(`routes/wrapper.tsx`, [
+    // Static routes
     index(`routes/home.tsx`),
     route(`about`, `routes/about.tsx`),
+    // Auth routes
     route(`sign-in`, `routes/signin.tsx`),
     route(`sign-up`, `routes/signup.tsx`),
     route(`sign-out`, `routes/signout.ts`),
-    route(`success`, `routes/customer-checkout-success.tsx`),
+    // Callback routes
+    route(`success`, `routes/success.tsx`),
+    // Protected routes
     layout(`routes/authenticated.tsx`, [
-      route(`checkout`, `routes/customer-checkout.ts`),
       route(`dashboard`, `routes/dashboard.tsx`),
-      route(`portal`, `routes/customer-portal.ts`),
       route(`profile`, `routes/profile.tsx`),
+      // Polar routes
+      route(`checkout`, `routes/polar/checkout.ts`),
+      route(`portal`, `routes/polar/portal.ts`),
+      // Admin routes
       ...prefix(`admin`, [
         route(`ai`, `routes/admin/ai.tsx`),
         route(`biz`, `routes/admin/business.tsx`),
         route(`email`, `routes/admin/email.tsx`),
       ]),
     ]),
+    // API routes
     ...prefix(`api`, [route(`auth/*`, `routes/api/auth.ts`)]),
   ]),
 ] satisfies RouteConfig;
